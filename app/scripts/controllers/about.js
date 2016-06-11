e0cba485fa8ae69cf893ef708ff103b361333f20
Jonathan Irvin <djfoxyslpr@gmail.com>
2016-06-10 20:35:44 -0500

    feat: scaffold using yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 523App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 523App
+ */
+angular.module('523App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 523App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 523App
+ */
+angular.module('523App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
