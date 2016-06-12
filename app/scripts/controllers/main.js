3ebbb83df16ee6d862bfd6126f120779a3fd05e4
Jonathan Irvin <djfoxyslpr@gmail.com>
2016-06-12 14:27:04 -0500

    feat: adjust responsive columns

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,5 +12,13 @@ angular.module('523App')
 MainCtrl.$inject = ['$scope'];
 
 function MainCtrl($scope) {
-  $scope.kelly = "Kelly Dayz";
+
+  // Welcome Message
+  $scope.welcomeHeader = "Welcome to Five Two Three!";
+  $scope.welcomeMessage = "We are the Irvins and over the years of being together, we've created a handful of websites centered in different areas. We have many websites: some personal, some professional, but all fun.";
+  $scope.whatHeader = "What is 523?";
+  $scope.whatMessage = "523 is what you get when an artist and a geek get married.";
+  $scope.thanksHeader = "Thanks for visiting!";
+  $scope.thanksMessage = "Feel free to scroll down and check out our network.";
+
 }
